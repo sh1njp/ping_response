26b24f931d3825b274292dc484c82349a96d4901
sh1njp <fnets.watanabe+githun@gmail.com>
2022-05-31 16:32:56 +0900

    Create get_ping_response.sh

---
+++ get_ping_response.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+## ping
+## ./
+
+## table name
+NAME="${1}"
+
+## ping destination
+DST="${2}"
+
+## ping clount
+COUNT="${3}"
+
+RESULT=`fping -c ${COUNT} ${DST} 2>&1 | tail -1`
+
+
+ping_xmt=`echo ${RESULT} | awk -F"[ /%]" '{print $8}'`
+ping_rcv=`echo ${RESULT} | awk -F"[ /%]" '{print $9}'`
+ping_loss=`echo ${RESULT} | awk -F"[ /%]" '{print $10}'`
+ping_min=`echo ${RESULT} | awk -F"[ /%]" '{print $16}'`
+ping_avg=`echo ${RESULT} | awk -F"[ /%]" '{print $17}'`
+ping_max=`echo ${RESULT} | awk -F"[ /%]" '{print $18}'`
+
+
+
+DB="db4"
+HOST="localhost"
+ROOM="none"
+
+curl -i -XPOST "http://localhost:8086/write?db=${DB}" \
+  --data-binary "${NAME},host=${HOST},room=${ROOM} ping_xmt=${ping_xmt},ping_rcv=${ping_rcv},ping_loss=${ping_loss},ping_min=${ping_min},ping_avg=${ping_avg},ping_max=${ping_max}"
